ee2b3e78469a7379e14ea3a1b8e28de4c32308e4
Patrick Müller <p.mueller@pcsg.de>
2017-01-30 13:56:56 +0100

    refactor: initial commit

---
+++ src/QUI/Auth/Google2Fa/Auth.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace QUI\Authe\Google2Fa;
+
+/**
+ * Class Auth
+ *
+ * Authentication handler for Google Authenticator
+ *
+ * @package QUI\Authe\Google2Fa
+ */
+class Auth
+{
+    public static function auth($authData)
+    {
+
+    }
+}
